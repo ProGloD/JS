14e7c439d0fe292dcea89dd71df567b2b239f046
Yaroslav Lashchenko <yariklashchenko@gmail.com>
2018-11-12 16:19:47 +0100

    advanced solution for "shopping_list"

--- exercises/week1/shopping_list/script.js
+++ exercises/week1/shopping_list/script.js
@@ -5,26 +5,40 @@ let output = document.querySelector("ul");
 let products = {};
 
 function add(e) {
-  let newItem = document.createElement("li");
-  let del = document.createElement("button");
-  del.addEventListener("click", deleteItem);
-  del.textContent = "Delete";
+  let key = product.value;
+  let number = parseInt(amount.value)
 
-  if (product in products) {
-    products[product] += parseInt(amount.value);
+  //if exist update value, else create new
+  if (key in products) {
+    products[key] += number;
   } else {
-    products[product] = parseInt(amount.value);
+    products[key] = number;
+  }
 
-    newItem.textContent = product.value;
+  // remove all li from ul
+  while (output.firstChild) {
+    output.removeChild(output.firstChild);
+  }
+
+  // reprint updated list
+  for (let p in products) {
+    let newItem = document.createElement("li");
+    let del = document.createElement("button");
+    del.addEventListener("click", deleteItem);
+    del.textContent = "Delete";
+    newItem.textContent = p + " " + products[p] + " ";
     newItem.appendChild(del);
 
     output.appendChild(newItem);
   }
+
 
   product.value = "";
   amount.value = "";
 }
 
 function deleteItem(e) {
+  let arr = e.target.parentNode.firstChild.textContent.split(" ");
+  delete products[arr[0]];
   e.target.parentNode.remove();
 }
